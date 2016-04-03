b9a50a00a2098a758ec14829c6daf01bc945bc43
Isak <aslundisak@gmail.com>
2016-04-03 23:21:21 +0200

    Redux provider added

--- client/client.js
+++ client/client.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom'
-import App from '../components/app'
+import { render } from 'react-dom';
+import App from '../components/app';
+import configureStore from '../redux/store';
+import { Provider } from 'react-redux';
 
-render(<App />, document.getElementById('app'));
+let initialState = {
+  unit: 'metrical'
+}
+
+let store = configureStore(initialState)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById('app')
+ );
